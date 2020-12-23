806bf59fe198f19b145be7b5f77888b9ea72a4a5
deadApex <matheusvieira0101@gmail.com>
2020-12-22 22:15:00 -0300

    Constantes em C
    Código tutorial de constantes em C

---
+++ Tutorials/C/constantes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+    #define DIGITO 1
+    #define NOME "Matheus"
+    #define CATEGORIA 'A'
+
+int main(void){
+    printf("O digito final do meu ID e: %d\n", DIGITO);
+    printf("E meu nome e: %s\n", NOME);
+    printf("E a categoria da minha carteira e: %c\n", CATEGORIA);
+    return 0;
+}
